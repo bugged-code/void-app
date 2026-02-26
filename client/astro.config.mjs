359f871f1ff4ef68c7a264aed977b08f97bb467d
Guss <guss.renteria@outlook.com>
2026-02-26 01:06:20 -0600

    [cliente] +init-dashboard

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   server: { port: 3000 },
@@ -10,4 +12,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
