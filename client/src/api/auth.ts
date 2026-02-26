d7e302b48165ed85af6202784b4f2e92e7a5f0da
Guss <guss.renteria@outlook.com>
2026-02-26 00:02:08 -0600

    [client] +session

---
+++ client/src/api/auth.ts
@@ -0,0 +1,30 @@
+const URL_BASE = "http://localhost:8000/auth";
+
+export const createAccount = async (username: string, password: string) => {
+  const res = await fetch(URL_BASE + "/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+
+  return res;
+};
+
+export const loginSession = async (username: string, password: string) => {
+  const res = await fetch(URL_BASE + "/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  return res;
+};
