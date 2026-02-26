646a890610a78238baaf26db367a20063ba01910
Guss <guss.renteria@outlook.com>
2026-02-26 00:16:24 -0600

    [server] +cookies

--- server/core/auth/auth.routes.ts
+++ server/core/auth/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { login, register } from "./auth.controller";
+import { login, logout, register } from "./auth.controller";
 import { AuthRequest, requireAuth } from "./auth.middleware";
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
-router.get("/me", requireAuth, (req: AuthRequest, res) => {
-  res.json({ message: "success", user: req.user });
+router.get("/profile", requireAuth, (req: AuthRequest, res) => {
+  res.json({ user: req.user });
 });
+router.get("/logout", logout);
 
 export default router;
